50470dab79b36945d9fa77c81066aef2e9e54c48
Andrey7287 <banderloginorama@gmail.com>
2017-06-06 09:58:12 +0300

    Update ymap.js
    edit ymap tpl

--- frontend/modules/ymap.js
+++ frontend/modules/ymap.js
@@ -1,29 +1,29 @@
-ymaps.ready(init);
-
 var map,
 		marker,
-		mobileView = window.matchMedia("(max-width: 768px)").matches;
+		center = [55.670133, 37.552427],
+		isMap = document.getElementById('map');
 
 function init(){
 
 	map = new ymaps.Map('map', {
-			center: mobileView ? [55.766426, 37.650840] : [55.766590, 37.654703],
-			zoom: 16,
-			controls: []
+			center: center,
+			zoom: 16
 	});
 
 
-	marker = new ymaps.Placemark(
-		[55.766426, 37.650840],{},{
-			iconLayout: 'default#image',
-			iconImageHref: '/bitrix/templates/site-template/images/map-marker.png',
-			iconImageSize: [40, 54],
-			iconImageOffset: [-20, -54]
-		});
+	marker = new ymaps.Placemark(center);
 
 	map.geoObjects.add(marker);
 	map.behaviors.disable('scrollZoom');
 	map.behaviors.disable('drag');
+	map.controls.add(
+	 new ymaps.control.ZoomControl()
+	);
+	map.controls.add(
+	 new ymaps.control.ToolBar()
+	);
 
-mapContainer ? ymaps.ready(init) : console.warn('Yandex map is not defined');
+}
+
+isMap && ymaps ? ymaps.ready(init) : console.log('There is no active map or API didn`t load');
 
